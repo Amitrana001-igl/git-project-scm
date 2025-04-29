5328f60ebf6eb27b78b87ae915fd161da6a4f122
ASHISHKUMAR9991 <ashishyadav0869@gmail.com>
2025-04-29 05:33:35 -0700

    These are my tags files6

---
+++ 2020_06_13_221522_add_error_and_should_sync_field_to_access_tokens.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddErrorAndShouldSyncFieldToAccessTokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('access_tokens', function (Blueprint $table): void {
+            $table->boolean('should_sync')->after('token')->default(1);
+            $table->text('error')->after('token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('access_tokens', function (Blueprint $table): void {
+        });
+    }
+}
